873a9d462dbd05807fd0f6e0c3f5780e966db64f
sr2977 <144753976+sr2977@users.noreply.github.com>
2023-11-27 22:53:03 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the 'public' folder
+app.use(express.static('public'));
+
+// GET route to serve the HTML form
+app.get('/cs212/lab7', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// POST route for handling form submission
+app.post('/cs212/lab7', (req, res) => {
+  const { noun1, adjective1, verb1, noun2, adjective2 } = req.body;
+
+  const madLib = `Once upon a time, a ${adjective1} ${noun1} decided to ${verb1} with a ${adjective2} ${noun2}.`;
+
+  res.send(madLib);
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
